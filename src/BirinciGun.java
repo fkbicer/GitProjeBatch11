b0a8441d76e5c7d10e48f4e0890c2d48f31986a8
fkbicer07 <74838110+fkbicer07@users.noreply.github.com>
2020-11-22 13:35:05 +0300

    Update BirinciGun.java

--- src/BirinciGun.java
+++ src/BirinciGun.java
@@ -7,5 +7,6 @@ public class BirinciGun {
 				System.out.println("10.11 ikinci gun	.");
 				System.out.println("10.26 ikinci gun ");
 				System.out.println("10.35 ikinci gun");
+				System.out.println("11.03 github.com");
 			}
 }

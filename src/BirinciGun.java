22c5b5100286c204a58073a588e41c9e46ca97fa
fkbicer07 <fkbicer07@gmail.com>
2020-11-22 13:28:17 +0300

    ikinci gün ikinci commit

---
+++ src/BirinciGun.java
@@ -0,0 +1,10 @@
+
+public class BirinciGun {
+	
+			public static void main(String[] args) {
+				System.out.println("11.57 --Birinci gun.");
+				System.out.println("Ikinci Commit");
+				System.out.println("10.11 ikinci gun	.");
+				System.out.println("10.26 ikinci gun ");
+			}
+}
--- src/BirinciGün.java
+++
@@ -1,9 +0,0 @@
-
-public class BirinciGün {
-	
-			public static void main(String[] args) {
-				System.out.println("11.57 --Birinci gün.");
-				System.out.println("İkinci Commit");
-				System.out.println("10.11 ikinci gün.")
-			}
-}
